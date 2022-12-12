6c4338cebbf8738123b4a6f4bf022498bab4bebb
Brian Shih <brianshihstart@gmail.com>
2022-12-11 21:43:55 -0800

    import rocksdb

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rocksdb::{Options, DB};
+
 fn main() {
+    let path = "test_path";
+    let db = DB::open_default(path).unwrap();
+    db.put(b"my key", b"my value").unwrap();
+    match db.get(b"my key") {
+        Ok(Some(value)) => println!("retrieved value {}", String::from_utf8(value).unwrap()),
+        Ok(None) => println!("value not found"),
+        Err(e) => println!("operational problem encountered: {}", e),
+    }
     println!("Hello, world!");
 }
